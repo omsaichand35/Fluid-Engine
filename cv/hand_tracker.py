0ea1b7445138959252329b0cbbfccb0c66172801
Harshith Daraboina <23bcs037@iiitdwd.ac.in>
2026-02-20 23:47:11 +0530

    Fix mediapipe solutions import

--- cv/hand_tracker.py
+++ cv/hand_tracker.py
@@ -1,17 +1,17 @@
 import cv2
-import mediapipe as mp
+import mediapipe.python.solutions as mp_solutions
 from config.settings import MIN_DETECTION_CONFIDENCE, MIN_TRACKING_CONFIDENCE
 
 class HandTracker:
     def __init__(self):
-        self.mp_hands = mp.solutions.hands
+        self.mp_hands = mp_solutions.hands
         self.hands = self.mp_hands.Hands(
             static_image_mode=False,
             max_num_hands=2,
             min_detection_confidence=MIN_DETECTION_CONFIDENCE,
             min_tracking_confidence=MIN_TRACKING_CONFIDENCE
         )
-        self.mp_draw = mp.solutions.drawing_utils
+        self.mp_draw = mp_solutions.drawing_utils
 
     def process_frame(self, frame):
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
